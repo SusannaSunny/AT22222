53e901f28335a8156d31067dedd1e84a9912ccf2
Susanna <susanna.demkina@gmail.com>
2022-06-05 06:39:00 +0300

    modified:   src/test/java/ru/netology/postman/Postman.java

--- src/test/java/ru/netology/postman/Postman.java
+++ src/test/java/ru/netology/postman/Postman.java
@@ -15,7 +15,7 @@ public class Postman {
         given()
                 .baseUri("https://postman-echo.com")
                 .contentType("text/plain; charset=UTF-8")
-                .body("some data")
+                .body("some data one")
                 .when()
                 .post("/post")
                 .then()
